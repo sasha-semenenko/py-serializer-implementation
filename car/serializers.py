a52e17976ede377b813be8f69fba92426a89f504
Sasha Semenenko <semenenko233249@gmail.com>
2023-10-07 13:47:43 +0300

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,34 @@
+from car.models import Car
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[MaxValueValidator(1914), MinValueValidator(1)]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        allow_null=True, allow_blank=True, required=False
+    )
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get(
+            "manufacturer", instance.manufacturer
+        )
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_power = validated_data.get(
+            "horse_power", instance.horse_power)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken
+                                                )
+        instance.problem_description = validated_data.get(
+            "problem_description", instance.problem_description
+        )
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import io
+
 from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    file_txt = io.BytesIO(json)
+    data = JSONParser().parse(file_txt)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return serializer.save()
